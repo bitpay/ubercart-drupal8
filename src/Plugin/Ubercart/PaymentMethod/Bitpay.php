48bc88dffff3077cda59e1906836319fd3cd3dd8
Joshua Lewis <joshlewis@gmail.com>
2019-09-23 11:09:38 -0400

    fixed redirect and hostname issues

--- src/Plugin/Ubercart/PaymentMethod/Bitpay.php
+++ src/Plugin/Ubercart/PaymentMethod/Bitpay.php
@@ -63,26 +63,17 @@ class Bitpay extends PaymentMethodPluginBase
      * {@inheritdoc}
      */
 
-    public function isSecure()
-    {
-        return
-            (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-            || $_SERVER['SERVER_PORT'] == 443;
-    }
+    
 
     public function orderSave(OrderInterface $order)
     {
+       
+        global $base_url;  
 
-        $http_host = $_SERVER['HTTP_HOST'];
-        if ($this->isSecure()):
-            $http_host = 'https://' . $http_host;
-        else:
-            $http_host = 'http://' . $http_host;
-        endif;
-
-        if ($_SERVER['REQUEST_URI'] == '/cart/checkout/complete' && strpos($_SERVER['HTTP_REFERER'], '/cart/checkout/review') != '') {
-
+        if (strpos($_SERVER['REQUEST_URI'],'/cart/checkout/complete') != '' && strpos($_SERVER['HTTP_REFERER'], '/cart/checkout/review') != '') {
+          
             $env_setting = \Drupal::config('bitpaycheckout.adminsettings')->get('bitpaycheckout_environment');
+           
 
             if ($env_setting == 1):
                 $env = 'prod';
@@ -91,6 +82,7 @@ class Bitpay extends PaymentMethodPluginBase
                 $env = 'test';
                 $bitpay_checkout_token = \Drupal::config('bitpaycheckout.adminsettings')->get('bitpaycheckout_devtoken');
             endif;
+            
 
             $config = new BPC_Configuration($bitpay_checkout_token, $env);
             $params = new \stdClass();
@@ -107,8 +99,8 @@ class Bitpay extends PaymentMethodPluginBase
             $params->price = $order_row->order_total;
             $params->currency = $order_row->currency; //set as needed
 
-            $params->redirectURL = $http_host . '/user/' . $order_row->uid . '/orders/' . $order_row->order_id;
-            $params->notificationURL = $http_host .'/bitpayipn';
+            $params->redirectURL = $base_url . '/user/' . $order_row->uid . '/orders/' . $order_row->order_id;
+            $params->notificationURL = $base_url .'/bitpayipn';
 
             if ($order_row->primary_email != ''):
                 $buyerInfo = new \stdClass();
